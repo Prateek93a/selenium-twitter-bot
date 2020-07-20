757f26f52dddef83b47b3c0b16cff9f20a73c350
Prateek93a <prateek93a@gmail.com>
2020-07-20 23:53:20 +0530

    made changes

---
+++ like_tweets_keyword.py
@@ -0,0 +1,12 @@
+import os
+from twitter_bot_class import TwitterBot
+
+if __name__ == "__main__":
+    try:
+        pj = TwitterBot(os.environ['EMAIL'], os.environ['PASSWORD'])
+        pj.login()
+        pj.search('100DaysOfCode')
+        pj.likeTweets(10)
+        pj.logout()
+    except Exception as e:
+        print(e)
---
+++ post_tweet.py
@@ -0,0 +1,12 @@
+import os
+from twitter_bot_class import TwitterBot
+
+if __name__ == "__main__":
+    try:
+        pj = TwitterBot(os.environ['EMAIL'], os.environ['PASSWORD'])
+        pj.login()
+        pj.postTweets("My bot's first tweet!")
+        pj.logout()
+    except Exception as e:
+        print(e)
+
